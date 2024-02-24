8cafbeb6507f73f4a0dd0c35d56a2d45d6687f89
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2024-02-24 14:04:24 +0100

    Realizacion de la documentacion del Ejercicio_5_8

--- src/main/kotlin/Ejercicio_5_8/Estudiante.kt
+++ src/main/kotlin/Ejercicio_5_8/Estudiante.kt
@@ -2,13 +2,26 @@ package Ejercicio_5_8
 
 import java.time.LocalDate
 
+/**
+ * Clase que representa a un estudiante, hereda de Persona y añade una propiedad carrera.
+ * @property carrera La carrera que estudia el estudiante.
+ * @constructor Crea una nueva instancia de Estudiante con el nombre, fecha de nacimiento
+ * y carrera especificados.
+ */
 class Estudiante(nombre: String, fechaNacimiento: LocalDate, var carrera: String)
     : Persona(nombre, fechaNacimiento) {
-
+    /**
+     * Sobrescritura del método toString para obtener una representación de cadena del
+     * estudiante.
+     * @return Una cadena que representa al estudiante con su nombre, edad y carrera.
+     */
     override fun toString(): String {
         return "${super.toString()}, carrera = $carrera"
     }
-
+    /**
+     * Sobrescritura del método actividad para mostrar la actividad específica de un
+     * estudiante.
+     */
     override fun actividad() {
         println("$nombre está estudiando $carrera.")
     }
--- src/main/kotlin/Ejercicio_5_8/Persona.kt
+++ src/main/kotlin/Ejercicio_5_8/Persona.kt
@@ -2,24 +2,43 @@ package Ejercicio_5_8
 
 import java.time.LocalDate
 
+/**
+ * Clase abierta que representa a una persona con nombre, fecha de nacimiento y edad.
+ * @property nombre El nombre de la persona.
+ * @property fechaNacimiento La fecha de nacimiento de la persona.
+ * @property edad La edad actual de la persona.
+ * @constructor Crea una nueva instancia de Persona con el nombre y fecha de nacimiento especificados.
+ */
 open class Persona(val nombre: String, fechaNacimiento: LocalDate) {
     private val fechaNacimiento: LocalDate = fechaNacimiento
 
     var edad = calcularEdad(fechaNacimiento)
-
+    /**
+     * Método privado que calcula la edad de la persona a partir de su fecha de nacimiento.
+     * @param fechaNacimiento La fecha de nacimiento de la persona.
+     * @return La edad actual de la persona.
+     */
     private fun calcularEdad(fechaNacimiento: LocalDate): Int {
         val fechaActual = LocalDate.now()
         return fechaActual.year - fechaNacimiento.year
     }
-
+    /**
+     * Método que incrementa la edad de la persona en un año.
+     */
     fun cumple() {
         edad++
     }
-
+    /**
+     * Método de sobrescritura del método toString para obtener una representación de
+     * cadena de la persona.
+     * @return Una cadena que representa a la persona con su nombre y edad.
+     */
     override fun toString(): String {
         return "Persona (nombre = $nombre, edad = $edad)"
     }
-
+    /**
+     * Método que representa la actividad genérica que realiza la persona.
+     */
     open fun actividad() {
         println("$nombre está realizando una actividad.")
     }
